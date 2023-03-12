b0435a8e6263e127f06384555eea6db1e724fad1
Hunter Harris <hlharris22@gmail.com>
2023-03-12 13:45:50 -0400

    Updated paddle

--- main.js
+++ main.js
@@ -6,46 +6,47 @@ class Main {
     this.x = 40
     this.y = 40
     this.fps = 60
-    this.Paddle = new Paddle(this.ctx, 250, 250, 40, 10)
+    this.Paddle = new Paddle(this.ctx, 250, 250, 40, 10, 3)
   }
 
   initialize() {
-    document.addEventListener('keydown', this.keyDownHandler, false)
-    document.addEventListener('keyup', this.keyUpHandler, false)
     setInterval(this.draw, 1000 / this.fps, this)
   }
 
   draw(main) {
-    console.log('Hello', main.x, main.y)
-    this.ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    this.ctx.font = '20px Arial'
-    this.ctx.fillStyle = '#000000'
-    this.ctx.fillText('hello', main.x, main.x)
+    main.ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    main.Paddle.draw()
+    main.ctx.font = '20px Arial'
+    main.ctx.fillStyle = '#000000'
+    main.ctx.fillText('hello', main.x, main.x)
+
+    if (main.rightPressed) {
+      main.Paddle.move(1)
+      main.x += 1
+    }
+    if (main.leftPressed) {
+      main.Paddle.move(-1)
+      main.x -= 1
+    }
   }
 
   keyDown(event) {
     if (event.code === 'ArrowRight') {
-      this.rightPressed = false
-      console.log('Pressing Right Down in class', this.x)
+      this.rightPressed = true
     }
 
     if (event.code === 'ArrowLeft') {
-      this.leftPressed = false
-      console.log('Pressing Left Down in class', this.x)
+      this.leftPressed = true
     }
   }
 
   keyUp(event) {
     if (event.code === 'ArrowRight') {
       this.rightPressed = false
-      this.x += 1
-      console.log('Pressing Right Up in class', this.x)
     }
 
     if (event.code === 'ArrowLeft') {
       this.leftPressed = false
-      this.x -= 1
-      console.log('Pressing Left Up in class', this.x)
     }
   }
 }
--- paddle.js
+++ paddle.js
@@ -1,13 +1,21 @@
 class Paddle {
-  constructor(ctx, x, y, width, height) {
+  constructor(ctx, x, y, width, height, speed) {
     this.ctx = ctx
     this.x = x
     this.y = y
     this.width = width
     this.height = height
+    this.speed = speed
   }
 
-  update() {}
+  draw() {
+    console.log('drawing')
+    this.ctx.font = '20px Arial'
+    this.ctx.fillStyle = '#000000'
+    this.ctx.fillText('hello', this.x, this.x)
+  }
 
-  move() {}
+  move(direction) {
+    this.x += this.speed * direction
+  }
 }
